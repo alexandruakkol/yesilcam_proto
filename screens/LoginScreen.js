8bb85afbddf1c06ae8e835d39ffa9cb5bf03db72
alexxme211 <92181364+alexxme211@users.noreply.github.com>
2022-05-11 16:31:48 +0300

    password show/hide not working

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,27 +6,65 @@ import {
   View,
 } from "react-native";
 import React from "react";
-import { NativeBaseProvider, Input, Button, KeyboardAvoidingView } from "native-base";
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import {
+  NativeBaseProvider,
+  Input,
+  Button,
+  KeyboardAvoidingView,
+  Icon,
+} from "native-base";
+import { useFonts, Jost_400Regular } from "@expo-google-fonts/jost";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const LoginScreen = () => {
+  //hooks
+  const [show, setShow] = React.useState(false);
 
   let [fontsLoaded] = useFonts({
-    Inter_900Black,
+    Jost_400Regular,
   });
 
   if (!fontsLoaded) {
     return <View />;
-} 
-else {
-
+  } else {
     return (
-      <NativeBaseProvider >
+      <NativeBaseProvider>
         <KeyboardAvoidingView behaviour="padding" style={styles.loginpage}>
           <Text style={styles.title}>Green Pine Connects</Text>
-          <Input style={styles.input} size="2xl" placeholder="email"></Input>
-          <Input secureTextEntry style={styles.input} placeholder="password"></Input>
-        <Button>Login</Button>
+          <View style={styles.inputContainer}>
+            <Input
+              style={styles.input}
+              variant="underlined"
+              size="2xl"
+              mx="4"
+              placeholder="Email"
+            ></Input>
+            <Input
+              secureTextEntry
+              variant="underlined"
+              style={styles.input}
+              mx="4"
+              marginTop="5"
+              size="2xl"
+              type={show ? "text" : "password"}
+              InputRightElement={
+                <Icon
+                  as={
+                    <MaterialIcons
+                      name={show ? "visibility" : "visibility-off"}
+                    />
+                  }
+                  size={5}
+                  mr="2"
+                  color="muted.400"
+                  onPress={() => setShow(!show)}
+                />
+              }
+              placeholder="Password"
+            ></Input>
+          </View>
+
+          <Button>Login</Button>
         </KeyboardAvoidingView>
       </NativeBaseProvider>
     );
@@ -37,21 +75,25 @@ export default LoginScreen;
 
 const styles = StyleSheet.create({
   loginpage: {
-    backgroundColor: "seagreen",
+    backgroundColor: "#32522f",
     flex: 1,
     justifyContent: "center",
     textAlign: "center",
     alignContent: "center",
   },
   title: {
-    fontFamily: "Inter_900Black",
+    fontFamily: "Jost_400Regular",
     justifyContent: "center",
     textAlign: "center",
     alignContent: "center",
     fontSize: 35,
-    marginBottom:200,
-    color:'beige',
+    marginBottom: 200,
+    color: "beige",
   },
 
-  input: {},
+  inputContainer: {},
+
+  input: {
+    fontSize: 17,
+  },
 });
