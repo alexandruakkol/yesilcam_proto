7eff1fddd7686a1afa2eb4323bc8a84eeb3719e8
alexandruakkol <alexandru.akkol@gmail.com>
2022-07-02 16:10:19 +0300

    persistent implementation

--- storage.js
+++ storage.js
@@ -3,6 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export const store = async (obj) => {
   try {
     await AsyncStorage.setItem(String(Object.keys(obj)), String(Object.values(obj)));
+    console.log('stored ',String(Object.keys(obj)), String(Object.values(obj)))
   } catch (e) {
     console.log("asyncstorage write error", e);
   }
@@ -10,7 +11,7 @@ export const store = async (obj) => {
 
 export const retrieve = async (key) => {
   try {
-    const value = await AsyncStorage.getItem(key).then((r)=>console.log(r));
+    const value = await AsyncStorage.getItem(key);
     return value != null ? value : null;
   } catch (e) {
     console.log("asyncstorage read error", e);
